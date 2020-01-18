094ec806ed126f1685c3ef9b5753d18de7e8fa7a
slevchyk <s.levchyk@gmail.com>
2020-01-18 02:44:11 +0200

    dropdown const

--- lib/contatns.dart
+++ lib/contatns.dart
@@ -8,3 +8,10 @@ const String KEY_SERVER_PASSWORD = "keyServerPassword";
 const String KEY_USER_ID = "keyUserID";
 const String KEY_USER_PIN = "keyUserPin";
 const String KEY_USER_PICTURE = "keyUserPicture";
+
+const String CS_SINGLE = 'Single';
+const String CS_MERRIED = 'Married';
+const String CS_DIVORCED = 'Divorced';
+const String CS_WIDOWED = 'Widowed';
+const String CS_OTHER = 'Other';
+
